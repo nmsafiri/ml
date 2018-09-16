8a94b434d4c21ed028b92a60209ccb2a92a8a5bd
nmsafiri <nicomsafirijm@gmail.com>
2018-09-16 13:28:01 -0400

    commit folder created

--- mlPractice/percept_1.py
+++ mlPractice/percept_1.py
@@ -9,7 +9,24 @@ class Perceptron(object):
   def fit(self, X, y): #X is an array defined by number of samples and features 
     self.w_ = np.zeros(1 + X.shape[1]) # creating an array with a size of number of weights + wo
     self.errors_ = []# a list where we will append all occurance of missclassification 
+    for _ in range(self.n_iter):
+        errors = 0
+        for xi, target in zip (X,y):
+            update = self.eta * (target - self.predict(xi))
+            self.w_[1:] += update * xi
+            self.w_[0] += update
+            errors += int(update != 0.0)
+        self.errors_.append(errors)
+    return self
+  
+  def net_input(self, X):
+    return np.dot(X, self.w_[1:]) + self.w_[0]
+
+  def predict(self,X):
+    return np.where(self.net_input(X) >= 0.0, 1, -1)
 
 
+   
 
-    
+
+ 
